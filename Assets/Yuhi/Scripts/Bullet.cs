36f0d63300c8b6fc196440cce91f4d8eb4a0c6a5
AkimotoYuhi <chiisanaaki@gmail.com>
2021-10-25 11:29:59 +0900

    画面外に出た弾が消えるようにした

---
+++ Assets/Yuhi/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    int m_speed = 1;
+    private Vector2 m_velo;
+    private Rigidbody2D m_rb;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_rb.velocity = new Vector2(1, 0);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Eria"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Yuhi/Scripts/Muzzle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muzzle : MonoBehaviour
+{
+    private enum Type
+    {
+        single,
+        way
+    }
+    [SerializeField] Type m_type = Type.single;
+    [SerializeField] float m_speed = 1f;
+    private GameObject m_player;
+
+    void Start()
+    {
+        m_player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (m_type == Type.single)
+        {
+
+        }
+        else if (m_type == Type.way)
+        {
+
+        }
+    }
+
+    private void Single()
+    {
+
+    }
+
+    private void Way()
+    {
+
+    }
+}
