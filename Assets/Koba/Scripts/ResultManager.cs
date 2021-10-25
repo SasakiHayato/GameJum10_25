5af1bdb0ac23bd07d9f1344d4e210a7d545099f5
K.Kobayashi <gunoka0629@gmail.com>
2021-10-25 16:30:40 +0900

    ランキング機能を追加

--- Assets/Koba/Scripts/ResultManager.cs
+++ Assets/Koba/Scripts/ResultManager.cs
@@ -2,32 +2,119 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using NCMB;
 
 public class ResultManager : MonoBehaviour
 {
     [SerializeField] Text m_scoreText;
     [SerializeField] Text m_rankingText;
+    [SerializeField] InputField m_nameInput;
+    [SerializeField] RectTransform m_entryPanel;
     [SerializeField] Text m_announceText;
+    [SerializeField] float m_gracePeriod = 10f;
     [SerializeField] SceneChanger m_sceneChanger;
 
-    float[] m_ranking;
+    List<NCMBObject> m_ranking;
+    int m_score;
     float m_timer;
+    bool m_closable = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         m_scoreText.text = GameManager.Instance().ResultScore.ToString();
+
+        MakeRankingText();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!m_closable)
+        {
+            m_timer += Time.deltaTime;
+
+            if (m_timer > m_gracePeriod)
+            {
+                m_closable = true;
+            }
+        }
+
         m_announceText.color = GetAlphaColor(m_announceText.color);
 
         if (Input.GetButtonDown("Jump"))
         {
             m_sceneChanger.ChangeScene();
         }
+    }
+
+  
+    public void GetRanking(int score)
+    {
+        m_score = score;
+
+        NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("GameJam1025_Ranking");
+        query.OrderByDescending("Score");
+        query.Limit = 5;
+
+        query.FindAsync((List<NCMBObject> objList, NCMBException e) =>
+        {
+            if (e != null)
+            {
+                Debug.LogError(e.ToString());
+            }
+            else
+            {
+                
+                m_ranking = objList;
+                MakeRankingText();
+
+                if ((score > 0 && m_ranking.Count < 5) || score > int.Parse(m_ranking[m_ranking.Count - 1]["Score"].ToString()) || m_ranking.Count == 0)
+                {
+                    m_entryPanel.gameObject.SetActive(true);
+                }
+            }
+        });
+    }
+
+
+    void MakeRankingText()
+    {
+        System.Text.StringBuilder builder = new System.Text.StringBuilder();
+        for (int i = 0; i < m_ranking.Count; i++)
+        {
+            builder.Append((i + 1).ToString());
+            builder.Append(", ");
+            builder.Append(m_ranking[i]["PlayerName"].ToString());
+            builder.Append(" : ");
+            builder.AppendLine(m_ranking[i]["Score"].ToString());
+        }
+        Debug.Log("Ranking Text:\r\n" + builder.ToString());
+        m_rankingText.text = builder.ToString();
+    }
+
+    public void SetScoreOfCurrentPlay(int score)
+    {
+        GetRanking(score);
+    }
+
+    public void Save()
+    {
+        NCMBObject ncmb = new NCMBObject("GameJam1025_Ranking");
+
+        ncmb["PlayerName"] = m_nameInput.text; ;
+        ncmb["Score"] = m_score;
+
+        ncmb.SaveAsync((NCMBException e) =>
+        {
+            if (e != null)
+            {
+                Debug.LogError(e.ErrorMessage);
+            }
+            else
+            {
+                Debug.Log("Save Successful");
+                GetRanking(m_score);
+            }
+        });
     }
 
     private Color GetAlphaColor(Color c)
