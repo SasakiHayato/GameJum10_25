3fe80ee2927fb7c3c3e8e1c7291e6df7a6fa128f
K.Kobayashi <gunoka0629@gmail.com>
2021-10-25 12:24:13 +0900

    リザルト画面のUIを配置

---
+++ Assets/Koba/ResultManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+    [SerializeField] Text m_scoreText;
+    [SerializeField] Text m_rankingText;
+    [SerializeField] Text m_announceText;
+    [SerializeField] SceneChanger m_sceneChanger;
+
+    float m_timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // m_scoreText.text = 
+
+        for (int i = 0; i < 5; i++)
+        {
+            // m_rankingText.text = i+1 + "、名前" + "　スコア"
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_announceText.color = GetAlphaColor(m_announceText.color);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_sceneChanger.ChangeScene();
+        }
+    }
+
+    private Color GetAlphaColor(Color c)
+    {
+        m_timer += Time.deltaTime * 3.0f;
+        c.a = Mathf.Sin(m_timer) * 6.0f;
+
+        return c;
+    }
+}
