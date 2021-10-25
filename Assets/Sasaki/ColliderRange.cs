6b2c880ace6034650f68c0de6f642001373cc1a8
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-10-25 11:36:07 +0900

    Playerの挙動、ステージ範囲の設定を追加

---
+++ Assets/Sasaki/ColliderRange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderRange : MonoBehaviour
+{
+    [SerializeField] float _maxRange;
+    [SerializeField] Transform[] _pos = new Transform[4];
+
+    BoxCollider2D[] _colliders = new BoxCollider2D[4];
+
+    private void Start()
+    {
+        SetUp();
+    }
+
+    void SetUp()
+    {
+        for (int count = 0; count < _pos.Length; count++)
+        {
+            _colliders[count] = gameObject.AddComponent<BoxCollider2D>();
+            _colliders[count].offset = _pos[count].position;
+
+            if (0 == count % 2) _colliders[count].size = new Vector2(1, _maxRange);
+            else _colliders[count].size = new Vector2(_maxRange, 1);
+        }
+    }
+}
